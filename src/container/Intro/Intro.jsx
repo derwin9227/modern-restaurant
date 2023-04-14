2cd60be11cd4f78be84dd2f95329a9244f5c38ee
derwin <derwin9227@gmail.com>
2023-04-14 14:06:15 -0500

    modify importing -bs- in containers-Intro

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BsFillPlayFill, BsPauseFill} from 'react-icons/Bs';
+import { BsFillPlayFill, BsPauseFill} from 'react-icons/bs';
 import { meal } from '../../constants';
 import './Intro.css';
 
